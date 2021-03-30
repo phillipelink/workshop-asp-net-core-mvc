502d3746c81af0a9d2ed304ba6987ad5e66adb46
Phillipe Marinho De Carvalho Guimaraes <pmarinho@indracompany.com>
2021-03-30 20:54:39 -0300

    Adionando Departamentos Models/Controller/View

---
+++ SistemaDeVendas/Controllers/DepartamentosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeVendas.Models;
+
+namespace SistemaDeVendas.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Departamento> list;
+            list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Eletronicos" });
+            list.Add(new Departamento { Id = 2, Nome = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
--- SistemaDeVendas/Models/ViewModels/ErrorViewModel.cs
+++ SistemaDeVendas/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SistemaDeVendas.Models
+namespace SistemaDeVendas.Models.ViewModels
 {
     public class ErrorViewModel
     {
